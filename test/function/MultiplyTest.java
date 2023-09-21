04d17f13f1f62c144fee9fd38e7a8ce71bdcae5c
Vera Ezeagu <veraeze18@gmail.com>
2023-09-21 09:28:10 +0100

    multiply test commit

--- test/function/MultiplyTest.java
+++ test/function/MultiplyTest.java
@@ -6,9 +6,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MultiplyTest {
     @Test
-    public  void  testThatTwoNumbersCanMultiply(){
+    public  void  twoPositiveNumbers(){
         int value = Function.multiply(7,7);
         assertEquals(49, value);
+    }
+
+    @Test
+    public  void  negativeSecondNumber(){
+        int value = Function.multiply(7,-5);
+        assertEquals(-35, value);
+    }
+
+    @Test
+    public  void  negativeFirstNumber(){
+        int value = Function.multiply(-4,5);
+        assertEquals(-20, value);
+    }
+
+    @Test
+    public  void  twoNegativeNumber(){
+        int value = Function.multiply(-2,-2);
+        assertEquals(4, value);
+    }
+
+    @Test
+    public  void  zeroFirstNumber(){
+        int value = Function.multiply(0,9);
+        assertEquals(0, value);
+    }
+
+    @Test
+    public  void  zeroSecondNumber(){
+        int value = Function.multiply(-10,0);
+        assertEquals(0, value);
+    }
+
+    @Test
+    public  void  bothNumbersZero(){
+        int value = Function.multiply(0,0);
+        assertEquals(0, value);
     }
 
 }
