183567df6547c1c7f04e06273f9c3d8c554ff23c
Vera Ezeagu <veraeze18@gmail.com>
2023-09-23 19:35:12 +0100

    star triangle java

---
+++ src/Pattern.java
@@ -0,0 +1,19 @@
+public class Pattern {
+    public static void main(String[] args) {
+        for (int rows = 1; rows <= 5; rows++) {
+
+            for (int column = 1; column <= rows - 1; column++) {
+
+                System.out.print("* ");
+
+            }
+            System.out.println(" ");
+        }
+        for (int rows = 1; rows <= 5; rows++) {
+            for (int line = 5; line >= rows ; line--) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
